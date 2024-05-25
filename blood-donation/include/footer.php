d57eda0fb9c9d13fd6dc13884e8f02edcf1cf578
Shaikh Ashahad <121104097+Ashahad02@users.noreply.github.com>
2024-05-25 16:50:45 +0530

    Add files via upload

---
+++ blood-donation/include/footer.php
@@ -0,0 +1,35 @@
+	<!-- contact us section -->
+	<div class="container-fluid">
+		<div class="row contactus">
+			<div class="col-md-6 offset-md-3">
+				<h1 class="text-center">Contact Us</h1>
+				<hr class="white-bar">
+
+				<h1 class="text-center">
+					<a target="_blank" href="ashahad.io"><i style="color: #fff;" class="ri-twitter-x-fill"></i></a>
+
+					<a target="_blank" href="#"><i style="color: #fff;" class="fab fa-youtube-square"></i></a>
+				</h1>
+
+				<div class="details" style="text-align: center;">
+					<p> <i class="ri-phone-fill"></i> 8855088817<br>
+					<i class="fa fa-envelope"></i> Bloodgroup@gmail.com  </p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- end contact us section -->
+
+	<!-- JQuery File -->
+	<script type="text/javascript" src="jquery/jquery.js"></script>
+
+	<!-- BootStrap JS File-->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Fontawesome Icon JS-->
+	<script defer src="fontawesome/js/all.js"></script>
+
+	</body>
+
+	</html>
---
+++ blood-donation/include/usernav.php
@@ -0,0 +1,31 @@
+<nav id="mainNav" class="navbar fixed-top navbar-default navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="./home.php">DONATETHEBLOOD</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+    </ul>
+
+    <ul class="navbar-nav form-inline my-2 my-lg-0">
+
+      <li class="nav-item">
+        <a class="nav-link" href="home.php">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="find_blood.php">Find Donor</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Be A Donor</a>
+      </li>
+
+
+    </ul>
+  </div>
+</nav>
